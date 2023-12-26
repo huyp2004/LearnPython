d57a823080a8b5246fc40a731d5e732127944a1b
Phan Nguyễn Quốc Huy <127077969+huyp2004@users.noreply.github.com>
2023-12-26 16:57:46 +0700

    Renew

--- oop/BaitapProperty/bai3.py
+++ oop/BaitapProperty/bai3.py
@@ -15,4 +15,57 @@ Test:
     Thay đổi tax_amount của đối tượng e thành 1500 -> Bob 10000 0.15 1500.0 8500.0
     Thay đổi net_salary của đối tượng e thành 8000 -> Bob 9411.764705882353 0.15 1411.764705882353 8000.0
     Xóa tax_amount của đối tượng e -> Bob 9411.764705882353 AttributeError: 'Employee' object has no attribute 'tax_rate'
-''' 
+'''
+
+from a import tax_amount
+
+
+class Employee:
+    def __init__(self, name, salary):
+        self.__name = name
+        self.__salary = salary
+        self.__tax_rate = 0.1
+
+    @property
+    def tax_rate(self):
+        return self.__tax_rate
+
+    @property
+    def tax_amount(self):
+        return self.__salary * self.__tax_rate
+
+    @tax_amount.setter
+    def tax_amount(self, value):
+        self.__tax_rate = value / self.__salary
+
+    @tax_amount.deleter
+    def tax_amount(self):
+        del self.__tax_rate
+
+    @property
+    def net_salary(self):
+        return self.__salary - self.tax_amount
+
+    @net_salary.setter
+    def net_salary(self, value):
+        self.__salary = value / (1 - self.__tax_rate)
+
+    @net_salary.deleter
+    def net_salary(self):
+        del self.__salary
+
+    def __str__(self):
+        return f"Name:{self.__name}, salary:{self.__salary}, tax_rate:{self.__tax_rate}, tax_amount:{self.tax_amount}, net_salary:{self.net_salary}"
+
+e = Employee("Huy", 10000)
+print(e)
+
+e.tax_amount = 1500
+print(e)
+
+e.net_salary = 8000
+print(e)
+
+del e.tax_amount
+
+del e.net_salary
